b31d788105fd48e9bc9e1c848a0e88d85acdbf08
sidorov <sidorov_a_a@t-soft.ru>
2025-11-20 22:32:42 +0300

    ya meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import {Html, Head, Main, NextScript} from 'next/document'
 export default function Document() {
     return (
         <Html>
-            <Head/>
+            <Head>
+                <meta name="yandex-verification" content="2854576f9ca54dcd" />
+            </Head>
             <body style={{margin: 0, "backgroundColor": "#14191C"}}>
             <Main/>
             <NextScript/>
