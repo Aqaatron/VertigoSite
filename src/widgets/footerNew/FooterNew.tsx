8df94af1dbc5f75573dd75c9b81da7917d35b854
sidorov <sidorov_a_a@t-soft.ru>
2026-03-22 21:22:22 +0300

    правки футера

--- src/widgets/footerNew/FooterNew.tsx
+++ src/widgets/footerNew/FooterNew.tsx
@@ -20,10 +20,10 @@ export const FooterNew = () => {
                 <div className={compStyles.title}>Vertigо Саратов</div>
                 <div className={compStyles.el}>Адрес: г. Саратов, проспект имени Петра  <p/>Столыпина 27 ТК Манеж, 3 этаж
                 </div>
-                <div className={compStyles.el}>Телефон:</div>
-                <div className={compStyles.el}>E-mail:</div>
+                <div className={compStyles.el}>Телефон: 89027100210</div>
+                <div className={compStyles.el}>E-mail: vertigosar64@gmail.com</div>
                 <div className={compStyles.el}>Часы работы:</div>
-                <div className={compStyles.el}>ПН - ВС: 10:00 - 21:00</div>
+                <div className={compStyles.el}>Ежедневно 10:00 - 21:00</div>
             </div>
             <iframe src="https://yandex.ru/map-widget/v1/?z=12&ol=biz&oid=103489904717" className={compStyles.map}
                     frameBorder="0"></iframe>
